9989362b448965cf271b218426cb705dd5cf1505
munenakoppa <30587898+munenakoppa@users.noreply.github.com>
2024-05-16 08:56:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './Compoments/Login';
+import Registration from './Compoments/Registration';
+import Menu from './Compoments/Menu';
+
+import Books from './Compoments/GraphQL/Books';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Menu />
+      {/*<Login />
+      <Books />*/}
+      
+    </div>
+  );
+}
+
+export default App;
